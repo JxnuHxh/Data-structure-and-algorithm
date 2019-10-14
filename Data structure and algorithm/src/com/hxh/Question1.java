16854917ab1a118da37c1f6b7fb83d83d3e9b75e
hxh <617662163@qq.com>
2019-10-14 11:16:50 +0800

    算法训练

---
+++ Data structure and algorithm/src/com/hxh/Question1.java
@@ -0,0 +1,20 @@
+package com.hxh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question1 {
+    private  static List<String > findMissingRanges(int[] nums,int lows,int upper)
+    {
+        //1 定义输出变量
+        List<String > rst=new ArrayList<>();
+        //2
+        if(nums=null||nums.length==0)
+        {
+            rst.add(lower+"->"+upper);
+        }
+        return rst;
+
+    }
+
+}
