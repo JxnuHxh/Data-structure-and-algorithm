fb48d2c653fbc67bd35b1465d603dc759f0d6da7
hxh <617662163@qq.com>
2019-11-23 14:10:31 +0800

    力扣

---
+++ Data structure and algorithm/src/com/cowguest/n004/Mian.java
@@ -0,0 +1,14 @@
+package com.cowguest.n004;
+
+import java.util.Scanner;
+
+public class Mian {
+    public static void main(String[] args) {
+        int s[] =new int[10001];
+        int prime[] =new int[10001];
+        Scanner sc=new Scanner(System.in);
+        int 
+
+
+    }
+}
---
+++ Data structure and algorithm/src/com/offer/Offer26.java
@@ -0,0 +1,30 @@
+package com.offer;
+
+import com.hxh.DFS.TreeNode;
+
+public class Offer26 {
+    public boolean HasSsubTree(TreeNode root1,TreeNode root2){
+        if(root1==null||root2==null){
+            return false;
+        }
+        if(root1.val==root2.val){
+            if(judge(root1,root2)){
+                return true;
+            }
+        }
+        return HasSsubTree(root1.left,root2)||HasSsubTree(root1.right,root2);
+    }
+
+    public boolean judge(TreeNode root,TreeNode subtree){
+        if(subtree == null){
+            return true;
+        }
+        if(root == null){
+            return false;
+        }
+        if(root.val==subtree.val){
+            return judge(root.left,subtree.left)&&judge(root.right,subtree.right);
+        }
+        return false;
+    }
+}
