113dfaa36f0410ee30784919552466de17192a82
JxnuHxh <617662163@qq.com>
2020-01-12 01:34:58 +0800

    链表转二叉平衡数

--- Data structure and algorithm/src/com/hxh/DG/ListNode.java
+++ Data structure and algorithm/src/com/hxh/DG/ListNode.java
@@ -2,8 +2,10 @@ package com.hxh.DG;
 
 public class ListNode {
 
-     int val;
-     ListNode next;
-     ListNode(int x) { val = x; }
+
+
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
 
 }
