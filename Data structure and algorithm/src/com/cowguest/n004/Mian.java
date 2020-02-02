bed57d662663a1504327724e4e416ede061f919a
JxnuHxh <617662163@qq.com>
2020-02-02 20:52:55 +0800

    内课外

--- Data structure and algorithm/src/com/cowguest/n004/Mian.java
+++ Data structure and algorithm/src/com/cowguest/n004/Mian.java
@@ -3,12 +3,8 @@ package com.cowguest.n004;
 import java.util.Scanner;
 
 public class Mian {
-    public static void main(String[] args) {
-        int s[] =new int[10001];
-        int prime[] =new int[10001];
-        Scanner sc=new Scanner(System.in);
-        int 
 
 
-    }
+
+
 }
