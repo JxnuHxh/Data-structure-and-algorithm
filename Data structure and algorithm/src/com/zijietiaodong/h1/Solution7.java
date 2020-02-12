33776952232706a438a09f9d72cfb3fb644f618d
JxnuHxh <617662163@qq.com>
2020-02-12 22:57:16 +0800

    节雨水

---
+++ Data structure and algorithm/src/com/zijietiaodong/h1/Solution7.java
@@ -0,0 +1,20 @@
+package com.zijietiaodong.h1;
+
+import com.hxh.DFS.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.zijietiaodong.h1
+ * @date 2020/2/11 1:13
+ */
+public class Solution7 {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<list<Integer>> w=new ArrayList();
+        List<Integer> n=new ArrayList();
+g
+    }
+}
