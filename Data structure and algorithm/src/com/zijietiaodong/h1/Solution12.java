1a153297ac3afa0058d4ea9fdecf5206e78d0652
JxnuHxh <617662163@qq.com>
2020-02-14 02:16:18 +0800

    分糖果

--- Data structure and algorithm/src/com/zijietiaodong/h1/Solution12.java
+++ Data structure and algorithm/src/com/zijietiaodong/h1/Solution12.java
@@ -14,7 +14,7 @@ public class Solution12 {
             int sum = 0;
             int[] left2right = new int[ratings.length];
             int[] right2left = new int[ratings.length];
-            Arrays.fill(left2right, 1);
+            Arrays.fill(left2right, 1);//用1填充数组
             Arrays.fill(right2left, 1);
             for (int i = 1; i < ratings.length; i++) {
                 if (ratings[i] > ratings[i - 1]) {
