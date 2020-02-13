787ac823759a5dc16c6098ba10b2325dfdeeebc7
JxnuHxh <617662163@qq.com>
2020-02-13 14:07:35 +0800

    提交

--- Data structure and algorithm/src/com/zijietiaodong/h1/Solution7.java
+++ Data structure and algorithm/src/com/zijietiaodong/h1/Solution7.java
@@ -12,9 +12,34 @@ import java.util.List;
  * @date 2020/2/11 1:13
  */
 public class Solution7 {
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<list<Integer>> w=new ArrayList();
-        List<Integer> n=new ArrayList();
-g
+    public int trap(int[] height) {
+        int sum = 0;
+        int max = getMax(height);//找到最大的高度，以便遍历。
+        for (int i = 1; i <= max; i++) {
+            boolean isStart = false; //标记是否开始更新 temp
+            int temp_sum = 0;
+            for (int j = 0; j < height.length; j++) {
+                if (isStart && height[j] < i) {
+                    temp_sum++;
+                }
+                if (height[j] >= i) {
+                    sum = sum + temp_sum;
+                    temp_sum = 0;
+                    isStart = true;
+                }
+            }
+        }
+        return sum;
     }
+    private int getMax(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > max) {
+                max = height[i];
+            }
+        }
+        return max;
+    }
+
+
 }
