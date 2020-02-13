85192bccfe7b97f208f1239a251be5a54c26298a
JxnuHxh <617662163@qq.com>
2020-02-14 02:08:48 +0800

    分糖果 动态规划

---
+++ Data structure and algorithm/src/com/zijietiaodong/h1/Solution12.java
@@ -0,0 +1,35 @@
+package com.zijietiaodong.h1;
+
+import java.util.Arrays;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.zijietiaodong.h1
+ * @date 2020/2/14 1:41
+ * 135分糖果
+ */
+public class Solution12 {
+        public int candy(int[] ratings) {
+            int sum = 0;
+            int[] left2right = new int[ratings.length];
+            int[] right2left = new int[ratings.length];
+            Arrays.fill(left2right, 1);
+            Arrays.fill(right2left, 1);
+            for (int i = 1; i < ratings.length; i++) {
+                if (ratings[i] > ratings[i - 1]) {
+                    left2right[i] = left2right[i - 1] + 1;
+                }
+            }
+            for (int i = ratings.length - 2; i >= 0; i--) {
+                if (ratings[i] > ratings[i + 1]) {
+                    right2left[i] = right2left[i + 1] + 1;
+                }
+            }
+            for (int i = 0; i < ratings.length; i++) {
+                sum += Math.max(left2right[i], right2left[i]);
+            }
+            return sum;
+        }
+    }
+
