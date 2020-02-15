8fc23da6a36a947513fc6309006cf6e2a597b36b
JxnuHxh <617662163@qq.com>
2020-02-16 00:55:28 +0800

    25 K个一组反转链表

---
+++ Data structure and algorithm/src/com/zijietiaodong/h1/Solution14.java
@@ -0,0 +1,41 @@
+package com.zijietiaodong.h1;
+
+import com.hxh.DG.ListNode;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.zijietiaodong.h1
+ * @date 2020/2/16 0:54
+ */
+public class Solution14 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) end = end.next;
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
