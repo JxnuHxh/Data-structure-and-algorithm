2f7c8ad48eaaadee2f8c80af0345215c15930614
JxnuHxh <617662163@qq.com>
2020-03-10 21:33:48 +0800

    提交

--- Data structure and algorithm/src/com/剑指Offer/Pro4.java
+++ Data structure and algorithm/src/com/剑指Offer/Pro4.java
@@ -8,7 +8,7 @@ package com.剑指Offer;
  */
 public class Pro4 {
         public boolean findNumberIn2DArray(int[][] matrix, int target) {
-            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            if ( matrix.length == 0 || matrix[0].length == 0) {
                 return false;
             }
             int rows = matrix.length, columns = matrix[0].length;
