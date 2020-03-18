96560f77b168583fe7040c7a3609d7e7ff6666c1
JxnuHxh <617662163@qq.com>
2020-03-18 23:26:30 +0800

    树的子结构

---
+++ Data structure and algorithm/src/com/剑指Offer/Problem26.java
@@ -0,0 +1,15 @@
+package com.剑指Offer;
+
+import com.hxh.DFS.TreeNode;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.剑指Offer
+ * @date 2020/3/18 23:25
+ */
+public class Problem26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+return true;
+    }
+}
