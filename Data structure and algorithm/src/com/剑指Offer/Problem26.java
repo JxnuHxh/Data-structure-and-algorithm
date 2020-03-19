497d673f083bbac4c1758d30c67335d10ae8d583
JxnuHxh <617662163@qq.com>
2020-03-19 11:04:59 +0800

    树的子结构

--- Data structure and algorithm/src/com/剑指Offer/Problem26.java
+++ Data structure and algorithm/src/com/剑指Offer/Problem26.java
@@ -9,7 +9,13 @@ import com.hxh.DFS.TreeNode;
  * @date 2020/3/18 23:25
  */
 public class Problem26 {
-    public boolean isSubStructure(TreeNode A, TreeNode B) {
-return true;
+    public boolean isSubStructure(TreeNode A, TreeNode B) {\
+        if(A==null||B==null)return false;
+        return recursion(A,B)||isSubStructure(A.left,B)||isSubStructure(A.right,B);
     }
+    boolean  recursion(TreeNode A,TreeNode B){
+        if(B==null)return true;
+        if(A==null)return false;
+        return A.val!=B.val && recursion(A.left,B.left) && recursion(A.right,B.right);
+ }
 }
