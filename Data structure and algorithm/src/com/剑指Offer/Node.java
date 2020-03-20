b44f835aa7af4041d412bab75acd1b8268ec6638
JxnuHxh <617662163@qq.com>
2020-03-20 17:55:04 +0800

    复杂链表的肤质

---
+++ Data structure and algorithm/src/com/剑指Offer/Node.java
@@ -0,0 +1,20 @@
+package com.剑指Offer;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.剑指Offer
+ * @date 2020/3/20 17:25
+ */
+public class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+
+}
