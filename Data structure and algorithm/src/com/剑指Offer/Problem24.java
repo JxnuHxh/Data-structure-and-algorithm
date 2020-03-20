3db8c8a68ad0b1fc4468d78d90800f37112b9496
JxnuHxh <617662163@qq.com>
2020-03-20 12:00:49 +0800

    删除链表结点

---
+++ Data structure and algorithm/src/com/剑指Offer/Problem24.java
@@ -0,0 +1,26 @@
+package com.剑指Offer;
+
+import com.hxh.DG.ListNode;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.剑指Offer
+ * @date 2020/3/20 11:15
+ */
+public class Problem24 {
+    public ListNode reverseList(ListNode head) {
+        if(head==null)return head;
+        ListNode p,q;
+        p=head.next;
+        head.next=null;
+        while(p!=null){
+            q=p;
+            p=p.next;
+            q.next=head;
+            head=q;
+
+        }
+        return head;
+    }
+}
