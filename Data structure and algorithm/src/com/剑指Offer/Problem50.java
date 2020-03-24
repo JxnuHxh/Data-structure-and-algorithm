3aaa2f8d831adab471bd02816d4eed1c1bbd0e13
JxnuHxh <617662163@qq.com>
2020-03-24 22:43:25 +0800

    只出现一次的字符

---
+++ Data structure and algorithm/src/com/剑指Offer/Problem50.java
@@ -0,0 +1,25 @@
+package com.剑指Offer;
+
+import java.util.HashMap;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.剑指Offer
+ * @date 2020/3/24 22:42
+ */
+public class Problem50
+{
+    public char firstUniqChar(String s) {
+        HashMap<Character, Integer> dic = new HashMap<>();
+        char[] sc = s.toCharArray();
+        for(char c : sc) {
+            if(!dic.containsKey(c)) dic.put(c, 1);
+            else dic.put(c, dic.get(c) + 1);
+        }
+        for(char c : sc) {
+            if(dic.get(c) == 1) return c;
+        }
+        return ' ';
+    }
+}
