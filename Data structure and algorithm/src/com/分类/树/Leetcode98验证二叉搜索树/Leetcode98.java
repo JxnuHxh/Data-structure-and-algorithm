c0e82a4a17e3b10a2c431deed27c2dcd467d8238
JxnuHxh <617662163@qq.com>
2020-03-31 01:02:52 +0800

    天秀验证二叉树

---
+++ Data structure and algorithm/src/com/分类/树/Leetcode98验证二叉搜索树/Leetcode98.java
@@ -0,0 +1,16 @@
+package com.分类.树.Leetcode98验证二叉搜索树;
+
+import com.hxh.DFS.TreeNode;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.分类.树.Leetcode98验证二叉搜索树
+ * @date 2020/3/31 0:47
+ */
+public class Leetcode98 {
+    private long prev = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        return (root == null) || (isValidBST(root.left) && prev < (prev = root.val) && isValidBST(root.right));
+    }
+}
