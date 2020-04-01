b6b8c12b6df53d454121e7040c4b772db41c9648
JxnuHxh <617662163@qq.com>
2020-04-01 21:26:04 +0800

    d

---
+++ Data structure and algorithm/src/com/分类/图/LeetCode130.java
@@ -0,0 +1,18 @@
+package com.分类.图;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.分类.图
+ * @date 2020/4/1 18:03
+ */
+public class LeetCode130 {
+    public void solve(char[][] board) {
+        int row=board.length-1;
+        int col=board[0].length-1;
+        for(int i=0;i<=row;i++){
+
+        }
+
+    }
+}
---
+++ Data structure and algorithm/src/com/分类/树/Leetcode111二叉树的最小深度/LeetCode111.java
@@ -0,0 +1,21 @@
+package com.分类.树.Leetcode111二叉树的最小深度;
+
+import com.hxh.DFS.TreeNode;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.分类.树.Leetcode111二叉树的最小深度
+ * @date 2020/4/1 16:28
+ */
+public class LeetCode111 {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        //1.如果左孩子和右孩子有为空的情况，直接返回m1+m2+1
+        //2.如果都不为空，返回较小深度+1
+        return (root.left == null || root.right == null) ? m1 + m2 + 1 : Math.min(m1,m2) + 1;
+
+    }
+}
