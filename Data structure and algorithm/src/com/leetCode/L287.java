520922c52f9e17beeb33f31c6f3b095c0c9eea72
JxnuHxh <617662163@qq.com>
2020-05-26 21:28:37 +0800

    yyi1

---
+++ Data structure and algorithm/src/com/leetCode/L287.java
@@ -0,0 +1,22 @@
+package com.leetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author atom.hu
+ * @version V1.0
+ * @Package com.leetCode
+ * @date 2020/5/26 16:39
+ */
+public class L287 {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+
+}
