f21349d968007d7d126c6ab37d0460cc086778ef
jxnuHxh <617662163@qq.com>
2020-06-21 23:42:44 +0800

    l14

--- Data structure and algorithm/src/com/leetCode/Leetcode20.java
+++ Data structure and algorithm/src/com/leetCode/Leetcode20.java
@@ -7,11 +7,16 @@ public class Leetcode20 {
     public boolean isValid(String s) {
         Stack<Character>stack = new Stack<>();
         for(char c: s.toCharArray()){
-            if(c=='(')stack.push(')');
-            else if(c=='[')stack.push(']');
-            else if(c=='{')stack.push('}');
-            else if(stack.isEmpty()||c!=stack.pop())return false;
+            if(c=='(') stack.push(')');
+            else if(c=='[') stack.push(']');
+            else if(c=='{') stack.push('}');
+            else if(stack.isEmpty()||c!=stack.pop()) return false;
         }
         return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Leetcode20 leetcode2=new Leetcode20();
+        leetcode2.isValid("((){}{}");
     }
 }
