0c84f5f73fe0b7432a5927337b24167318b7c913
jxnuHxh <617662163@qq.com>
2020-06-24 23:45:16 +0800

    saunfa

--- Data structure and algorithm/src/com/leetCode/Leetcode16.java
+++ Data structure and algorithm/src/com/leetCode/Leetcode16.java
@@ -1,26 +1,27 @@
 package com.leetCode;
 
 import java.util.Arrays;
+
 //最接近的三数之和
 public class Leetcode16 {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int ans = nums[0] + nums[1] + nums[2];
-        for(int i=0;i<nums.length;i++) {
-            int start = i+1, end = nums.length - 1;
-            while(start < end) {
-                int sum = nums[start] + nums[end] + nums[i];
-
-                if(Math.abs(target - sum) < Math.abs(target - ans))
-                    ans = sum;
-                if(sum > target)
-                    end--;
-                else if(sum < target)
+        int res=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<nums.length;i++){
+            int start=i+1,end=nums.length-1;
+            while(start<end){
+                int sum=nums[i]+nums[start]+nums[end];
+                if(Math.abs(sum-target)<Math.abs(res-target)){
+                    res=sum;
+                } else if(sum<target){
                     start++;
-                else
-                    return ans;
+                }else if(sum>target){
+                    end--;
+                }else{
+                    return res;
+                }
             }
         }
-        return ans;
+        return res;
     }
 }
---
+++ Data structure and algorithm/src/com/leetCode/Main.java
@@ -0,0 +1,8 @@
+package com.leetCode;
+
+public class Main {
+    public static void main(String[] args) {
+        Leetcode70 leetcode70=new Leetcode70();
+        leetcode70.climbStairs(4);
+    }
+}
