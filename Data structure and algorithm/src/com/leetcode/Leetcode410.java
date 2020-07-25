53224e4e098047c7d741cd0e41174b025da1af4c
黄小华 <huangxiaohua@huangxiaohuadeMacBook-Pro.local>
2020-07-25 15:40:55 +0800

    410

---
+++ Data structure and algorithm/src/com/leetcode/Leetcode410.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+public class Leetcode410 {
+    public int splitArray(int[] nums, int m) {
+        int left = 0, right = 0;
+        for (int i = 0; i < nums.length; i++) {
+            right += nums[i];
+            if (left < nums[i]) {
+                left = nums[i];
+            }
+        }
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (check(nums, mid, m)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean check(int[] nums, int x, int m) {
+        int sum = 0;
+        int cnt = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum + nums[i] > x) {
+                cnt++;
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+        }
+        return cnt <= m;
+    }
+}
