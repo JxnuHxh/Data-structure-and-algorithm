0f34dddb589a765abe50eec9fec33daf5b247764
黄小华 <huangxiaohua@huangxiaohuadeMacBook-Pro.local>
2020-08-23 03:18:29 +0800

    快排

--- Data structure and algorithm/src/com/排序算法/Qk.java
+++ Data structure and algorithm/src/com/排序算法/Qk.java
@@ -1,27 +1,48 @@
 package com.排序算法;
-//快排单边循环
+//快排单边扫描
 public class Qk {
     public static void main(String[] args) {
-        int[] a=new int[]{1,2,9,2,3,89};
-        Qk.kp(0,a.length-1,a);
+        int[] a=new int[]{3,1,2,9,2,3,89};
+        Qk.kps(0,a.length-1,a);
         for(int c:a){
             System.out.print(c+" ");
         }
     }
-    private static void   kp(int start,int end,int[] nums){
-        if(start>end) return ;
-        int base=nums[start],count=start;
-        for(int i=start+1;i<=end&&count<end;i++) {
-            if (nums[i] <base) {
+    //单边扫描
+    private static void  kpd(int start,int end,int[] nums){
+        if(start>end) return;
+        int count=start;
+        int base=nums[start];
+        for(int i=start+1;i<=end;i++){
+            if(nums[i]<base){
                 count++;
-                int temp=nums[count];
+                int temp = nums[count];
                 nums[count]=nums[i];
                 nums[i]=temp;
             }
         }
-        nums[start] = nums[count];
-        nums[count] = base;
-        kp(start,count-1,nums);
-        kp(count+1,end,nums);
+        nums[start]=nums[count];
+        nums[count]=base;
+        kpd(start,count-1,nums);
+        kpd(count+1,end,nums);
+    }
+    //双边扫描
+    private static void kps(int start,int end,int[] nums){
+        if(start>end) return;
+        int base=nums[start];
+        int i=start,j=end;
+        while (i!=j){
+            while (base<nums[j]&&i<j)j--;
+            while (base>=nums[i]&&i<j)i++;
+            if(i<j){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+            }
+        }
+        nums[start]=nums[i];
+        nums[i]=base;
+        kps(start,i-1,nums);
+        kps(i+1,end,nums);
     }
 }
